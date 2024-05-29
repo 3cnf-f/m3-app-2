9eb87cad13132a8b7f045839216695e4e0aa58fd
farouk.hashim@gmail.com <farouk.hashim@gmail.com>
2024-05-29 09:46:13 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -5,8 +5,10 @@ class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.timer_1.interval=0.1
+    self.timer_1.interval=1
     self.counter=0
+    self.oldcounter=0
+    self.increm=0
     # Any code you write here will run before the form opens.
 
   def button_1_click(self, **event_args):
@@ -18,14 +20,28 @@ class Form1(Form1Template):
     if (self.counter>self.oldcounter):
       self.text_box_2.text=self.text_box_1.text+str(self.counter)
 
-    self.old
+    self.oldcounter=self.counter
    
     
     pass
 
   def timer_1_tick(self, **event_args):
     "This method is called Every [interval] seconds. Does not trigger if [interval] is 0."""
-    self.counter=self.counter+0.5
+    self.counter=self.counter+1
     
+    
+    pass
+
+  def text_box_3_change(self, **event_args):
+    
+    self.increm=0
+    while (self.increm<0):  
+      
+      try:
+        self.increm=1
+        self.timer_1.interval=float(self.text_box_3.text)
+        break
+      except ValueError:
+        pass
     
     pass
